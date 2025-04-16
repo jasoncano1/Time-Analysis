0fe7915c2567dd144941bbce078a6c2f83bac437
Jason Cano <jasoncano0509@gmail.com>
2025-04-16 10:03:23 -0500

    added analysis link and plotly line chart

--- public/js/scheduler.js
+++ public/js/scheduler.js
@@ -301,5 +301,27 @@ signOut.onclick = () => {
   window.location.href = "/";
 };
 
+analysis.onclick = () => {
+  main.classList.toggle("slideLeftOut", true);
+  setTimeout(() => {
+    
+    main.innerHTML = "";
+
+    var data = [
+      {
+        x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+        y: [1, 3, 6],
+        type: 'scatter'
+      }
+    ];
+    
+    Plotly.newPlot('main', data);
+    
+  }, 500);
+  setTimeout(() => {
+    main.classList.toggle("slideLeftOut", false);
+  }, 1000);
+}
+
 nextWk.onclick = () => renderNextWeek(nextMonday);
 prevWk.onclick = () => renderPreviousWeek(nextMonday);
