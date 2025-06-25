8c03c320c8758d1efb83c2be4dc0ee154d67c42e
Jason Cano <jasoncano0509@gmail.com>
2025-06-25 09:39:03 -0500

    added connection to render and user info to dotenv file

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,24 @@
+let pool;
 require('dotenv').config();
 const { Pool } = require('pg');
 
-// Create a new pool instance with your PostgreSQL configuration.
-const pool = new Pool({
-  user: 'postgres',           // Database user
-  host: 'localhost',          // Database host
-  database: 'users_db',         // Your database name
-  password: "Ace908098!",   // Database password
-  port: 5432,                 // Database port
-});
+if (process.env.DATABASE_URL) {
+  const connectionString = process.env.DATABASE_URL;
+  pool = new Pool({
+    connectionString: connectionString,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+  });
+}
 
 const connectToDb = async () => {
   try {
